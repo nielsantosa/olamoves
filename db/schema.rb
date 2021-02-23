6e0a253ec9484fdf3747838bd247838b34fcf4bf
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-23 23:13:31 +0800

    Add orders columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_095118) do
+ActiveRecord::Schema.define(version: 2021_02_23_150954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_02_23_095118) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "video_id"
+    t.bigint "user_id"
+    t.bigint "purchase_id"
+    t.boolean "confirmed", default: false
+    t.index ["purchase_id"], name: "index_orders_on_purchase_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.index ["video_id"], name: "index_orders_on_video_id"
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -48,4 +55,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_095118) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "purchases"
+  add_foreign_key "orders", "users"
+  add_foreign_key "orders", "videos"
 end
