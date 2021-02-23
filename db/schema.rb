e1f1180261739a9e0a732f900d8d362b01e1175a
timothy cheong <themomentisnow123@outlook.com>
2021-02-23 17:28:28 +0800

    Create purchase model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_074407) do
+ActiveRecord::Schema.define(version: 2021_02_23_092655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "purchases", force: :cascade do |t|
+    t.integer "total_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
