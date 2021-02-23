f2b472c309b1e406c49f01a2d1de8fbff5f43f67
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-23 23:41:46 +0800

    add routes and configure devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :videos, only: [:index, :show]
+
+  get '/cart', to: 'orders#checkout'
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 end
