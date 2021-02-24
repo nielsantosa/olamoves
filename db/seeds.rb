0262beafa9addb06fce9df32c8502cd14a7e1cec
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-24 12:36:43 +0800

    Create seeding file for videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "Start seeding 🍑"
 
+puts "Cleaning videos data 🧹"
+Video.destroy_all
 
 10.times do
   title = Faker::Restaurant.name
@@ -7,6 +9,15 @@ puts "Start seeding 🍑"
   price = (0..200).step(10).to_a.sample
   difficulty = (1..5).to_a.sample
   duration = (5..20).to_a.sample
+  video_url = "video_url"
+  puts "Creating video #{title}"
+  Video.create!(
+    title: title,
+    description: description,
+    price: price,
+    difficulty: difficulty,
+    duration: duration,
+    video_url: video_url)
 end
 
 puts "Finish seeding 🍑"
