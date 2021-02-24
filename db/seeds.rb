172aa16fd9f53b22ae880c58d359776ccc1684cb
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-24 14:54:01 +0800

    Configure video seed algo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,23 @@ def generate_pexel_video
   client = Pexels::Client.new(pexels_key) # Set up the client
 
   pexels_json_parsed = client.videos.search('yoga') # Search for videos with keyword "waves", return json files with 15 results
-  video_url = pexels_json_parsed.videos[0].files[0].link # Choose the first video, take the first video file type (there are HD, SD, etc. options), then take the url link
-  video_type = pexels_json_parsed.videos[0].files[0].file_type # Similar to line above, but take the video type
-  [video_url, video_type]
+  videos = pexels_json_parsed.videos
+  # video_url = pexels_json_parsed.videos[0].files[0].link # Choose the first video, take the first video file type (there are HD, SD, etc. options), then take the url link
+  # video_type = pexels_json_parsed.videos[0].files[0].file_type # Similar to line above, but take the video type
+  # [video_url, video_type]
 end
 
-video_url, video_type = generate_pexel_video
+videos = generate_pexel_video
 
-10.times do
+(0..10).to_a.each do |i|
   title = Faker::Restaurant.name
   description = Faker::Restaurant.description
   price = (0..200).step(10).to_a.sample
   difficulty = (1..5).to_a.sample
   duration = (5..20).to_a.sample
+  video_url = videos[i].files[0].link
+  video_type = videos[i].files[0].file_type
+
   puts "Creating video #{title}"
   Video.create!(
     title: title,
@@ -31,5 +35,8 @@ video_url, video_type = generate_pexel_video
     video_url: video_url,
     video_type: video_type)
 end
+
+puts "Creating Users 💁‍♀️"
+
 
 puts "Finish seeding 🍑"
