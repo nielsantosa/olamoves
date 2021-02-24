c94c2a402fe6f766bd806a6077e1cc2ac657bd74
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-24 12:29:52 +0800

    Adding yoga_video.mp4 to assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 puts "Start seeding 🍑"
 
-puts "Adding video"
+
+10.times do
+  title = Faker::Restaurant.name
+  description = Faker::Restaurant.description
+  price = (0..200).step(10).to_a.sample
+  difficulty = (1..5).to_a.sample
+  duration = (5..20).to_a.sample
+end
 
 puts "Finish seeding 🍑"
