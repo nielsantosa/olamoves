8eaaf90f00bdbfe3a61de5f44c879fd7b25ef1b9
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-24 17:38:44 +0800

    Complete checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,14 @@
 class OrdersController < ApplicationController
   def checkout
+    @user = current_user
+    @purchased_order = Order.where(user: @user, confirmed: false)
+    @purchased_videos = @purchased_order.map do |order|
+      order.video
+    end
+
+    @total_price = 0
+    @purchased_videos.each do |video|
+      @total_price += video.price
+    end
   end
 end
