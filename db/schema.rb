bac4a7c3e52a7af41afde8b594b60baa06d27025
Regina Soh <syzregina@gmail.com>
2021-02-26 11:35:13 +0800

    Add association for goal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_031959) do
+ActiveRecord::Schema.define(version: 2021_02_26_033256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_02_26_031959) do
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -87,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_02_26_031959) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "goals", "users"
   add_foreign_key "orders", "purchases"
   add_foreign_key "orders", "users"
   add_foreign_key "orders", "videos"
--- db/seeds.rb
+++ db/seeds.rb
@@ -87,6 +87,13 @@ puts "Creating Users - start"
     puts "Creating unconfirmed orders - #{i}"
     Order.create!(video: chosen_video, user: user, confirmed: false)
   end
+
+  puts "Creating Unconfirmed Goals - start"
+  (1..4).to_a.each do |i|
+
+    puts "Creating unconfirmed orders - #{i}"
+    Goal.create!(description: Faker::Hipster.sentence(word_count: 3, supplemental: true, random_words_to_add: 0, open_compounds_allowed: false))
+  end
 end
 
 
