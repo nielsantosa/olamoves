36061e63b388fbce192b8e70727a58e0e4217916
Cheyenne Lu <cheyenne.lu@gmail.com>
2021-02-26 14:24:58 +0800

    Instructor live

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_060449) do
+ActiveRecord::Schema.define(version: 2021_02_26_061620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_060449) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.boolean "instructor", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -77,10 +78,12 @@ ActiveRecord::Schema.define(version: 2021_02_24_060449) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "video_url"
     t.string "video_type"
+    t.integer "user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "orders", "purchases"
   add_foreign_key "orders", "users"
   add_foreign_key "orders", "videos"
+  add_foreign_key "videos", "users"
 end
