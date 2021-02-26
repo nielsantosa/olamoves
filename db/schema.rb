21192d9111baada1566f36c08d661acff4a0772d
timothy cheong <themomentisnow123@outlook.com>
2021-02-26 13:31:11 +0800

    before lunch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_060449) do
+ActiveRecord::Schema.define(version: 2021_02_26_041034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,17 @@ ActiveRecord::Schema.define(version: 2021_02_24_060449) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "video_id", null: false
+    t.bigint "user_id", null: false
+    t.text "comment"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["video_id"], name: "index_reviews_on_video_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -83,4 +94,6 @@ ActiveRecord::Schema.define(version: 2021_02_24_060449) do
   add_foreign_key "orders", "purchases"
   add_foreign_key "orders", "users"
   add_foreign_key "orders", "videos"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "videos"
 end
