eb0d5d589590e279bbb149f02145069c09fb78f8
timothy cheong <themomentisnow123@outlook.com>
2021-02-26 18:17:25 +0800

    done with create review, left delete review function

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :video
   belongs_to :user
   validates :comment, presence: true
-  validates :rating, length: { in: 1..5 }
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
